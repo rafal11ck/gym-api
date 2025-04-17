f759d8a33a17031703b1f86b578fb2af08739a78
rafal <you@example.com>
2025-04-17 13:10:32 +0200

    chore: fix test

--- src/src/test/java/xyz/cursedman/gym_api/controllers/TargetMuscleControllerTest.java
+++ src/src/test/java/xyz/cursedman/gym_api/controllers/TargetMuscleControllerTest.java
@@ -1,22 +1,29 @@
 package xyz.cursedman.gym_api.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Order;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import xyz.cursedman.gym_api.domain.dtos.targetMuscle.CreateTargetMuscleRequest;
+
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TargetMuscleControllerTest {
 
 	@Autowired
@@ -27,54 +34,32 @@ class TargetMuscleControllerTest {
 
 
 	@Test
-	void testExercisesGet() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/target-muscles"))
-			.andDo(print())
-			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
-	}
+	void testCrud() throws Exception {
 
-	@Test
-	@Order(1)
-	void createTargetMuscle() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.post("/target-muscles")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(
-					objectMapper.writeValueAsString(CreateTargetMuscleRequest.builder()
-						.name("ass")
-						.build())
-				))
-			.andDo(print())
-			.andExpect(MockMvcResultMatchers.status().isCreated())
-			.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+	// add
+	MvcResult addResult = mockMvc.perform(MockMvcRequestBuilders.post("/target-muscles")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(
+				objectMapper.writeValueAsString(CreateTargetMuscleRequest.builder()
+					.name("ass")
+					.build())
+			))
+		.andExpect(MockMvcResultMatchers.status().isCreated())
+		.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+		.andReturn();
 
-		mockMvc.perform(MockMvcRequestBuilders.get("/target-muscles")).andDo(print())
-			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("ass"));
-	}
+	String uuid = JsonPath.read(addResult.getResponse().getContentAsString(), "$.uuid");
 
-	@Test
-	@Order(2)
-	void deleteTargetMuscle() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.post("/target-muscles")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(
-					objectMapper.writeValueAsString(CreateTargetMuscleRequest.builder()
-						.name("ass")
-						.build())
-				))
-			.andDo(print())
-			.andExpect(MockMvcResultMatchers.status().isCreated())
-			.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
-		
-		String getResponse = mockMvc.perform(MockMvcRequestBuilders.get(
-			"/target-muscles")).andDo(print()).andReturn().getResponse().getContentAsString();
+	// get
+	mockMvc.perform(MockMvcRequestBuilders.get("/target-muscles"))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("ass"));
 
-		System.out.println(getResponse);
-//
-//		mockMvc.perform(
-//			MockMvcRequestBuilders.delete("/target-muscles/" + JsonPath.read(list, "$[0].name")));
+
+	// delete
+	mockMvc.perform(MockMvcRequestBuilders.delete("/target-muscles/{uuid}", uuid))
+		.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
 
 }
