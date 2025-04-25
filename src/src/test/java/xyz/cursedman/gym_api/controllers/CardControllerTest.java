868c2b930b8037bb8aa5b2ace8c2d322d0d948a7
Filip Stępień <filip.stepien30@gmail.com>
2025-04-25 14:11:18 +0200

    refactor: extract endpoint uri to field
    easier to change later on

--- src/src/test/java/xyz/cursedman/gym_api/controllers/CardControllerTest.java
+++ src/src/test/java/xyz/cursedman/gym_api/controllers/CardControllerTest.java
@@ -29,6 +29,8 @@ class CardControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	private final String endpointUri = "/cards";
+
 	private final String validCardUuid = "5bd05494-155e-4bd1-b14c-61421d0caaae";
 
 	private final String validCountryUuid = "352ed7f1-8bb1-4baa-9ca7-88995ec58d8a";
@@ -46,21 +48,21 @@ class CardControllerTest {
 
 	@Test
 	void checkIfGetReturnsHttp200AndAllRecords() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/cards"))
+		mockMvc.perform(MockMvcRequestBuilders.get(endpointUri))
 			.andExpect(MockMvcResultMatchers.status().isOk())
 			.andExpect(MockMvcResultMatchers.jsonPath("$.length()", Matchers.greaterThan(0)));
 	}
 
 	@Test
 	void checkIfGetByIdReturnsHttp200AndRequestedRecord() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/cards/" + validCardUuid))
+		mockMvc.perform(MockMvcRequestBuilders.get(endpointUri + "/" + validCardUuid))
 			.andExpect(MockMvcResultMatchers.status().isOk())
 			.andExpect(MockMvcResultMatchers.jsonPath("$").exists());
 	}
 
 	@Test
 	void checkIfGetNonExistingRecordReturns404AndEmptyBody() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/cards/" + UUID.randomUUID()))
+		mockMvc.perform(MockMvcRequestBuilders.get(endpointUri + "/" + UUID.randomUUID()))
 			.andExpect(MockMvcResultMatchers.status().isNotFound())
 			.andExpect(MockMvcResultMatchers.jsonPath("$").doesNotExist());
 	}
@@ -70,7 +72,7 @@ class CardControllerTest {
 	@Test
 	void checkIfCreateReturnsHttp201AndCreatedRecord() throws Exception {
 		mockMvc.perform(
-			MockMvcRequestBuilders.post("/cards")
+			MockMvcRequestBuilders.post(endpointUri)
 			.contentType(MediaType.APPLICATION_JSON)
 			.content(TestJsonHelper.stringify(validCardRequest))
 		).andExpect(MockMvcResultMatchers.status().isCreated())
@@ -83,7 +85,7 @@ class CardControllerTest {
 	@Test
 	void checkIfInvalidCreateBodyReturnsHttp400() throws Exception {
 		mockMvc.perform(
-			MockMvcRequestBuilders.post("/cards")
+			MockMvcRequestBuilders.post(endpointUri)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content("{}")
 		).andExpect(MockMvcResultMatchers.status().isBadRequest());
@@ -94,7 +96,7 @@ class CardControllerTest {
 	@Test
 	void checkIfPatchUpdateReturnsHttp200AndUpdatedRecord() throws Exception {
 		mockMvc.perform(
-			MockMvcRequestBuilders.patch("/cards/" + validCardUuid)
+			MockMvcRequestBuilders.patch(endpointUri + "/" + validCardUuid)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(TestJsonHelper.stringify(validCardRequest))
 		).andExpect(MockMvcResultMatchers.status().isOk())
@@ -107,7 +109,7 @@ class CardControllerTest {
 	@Test
 	void checkIfPatchUpdateOfNonExistingRecordReturnsHttp404() throws Exception {
 		mockMvc.perform(
-			MockMvcRequestBuilders.patch("/cards/" + UUID.randomUUID())
+			MockMvcRequestBuilders.patch(endpointUri + "/" + UUID.randomUUID())
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(TestJsonHelper.stringify(validCardRequest))
 		).andExpect(MockMvcResultMatchers.status().isNotFound());
@@ -116,7 +118,7 @@ class CardControllerTest {
 	@Test
 	void checkIfInvalidPatchUpdateBodyReturnsHttp400() throws Exception {
 		mockMvc.perform(
-			MockMvcRequestBuilders.patch("/cards/" + validCardUuid)
+			MockMvcRequestBuilders.patch(endpointUri + "/" + validCardUuid)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content("{}")
 		).andExpect(MockMvcResultMatchers.status().isBadRequest());
