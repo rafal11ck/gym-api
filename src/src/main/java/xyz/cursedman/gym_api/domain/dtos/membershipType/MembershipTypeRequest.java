39879f59d4eeec84072cb3773ba893202b5430df
Filip Stępień <filip.stepien30@gmail.com>
2025-04-25 14:01:17 +0200

    test: membership type tests

--- src/src/main/java/xyz/cursedman/gym_api/domain/dtos/membershipType/MembershipTypeRequest.java
+++ src/src/main/java/xyz/cursedman/gym_api/domain/dtos/membershipType/MembershipTypeRequest.java
@@ -1,5 +1,6 @@
 package xyz.cursedman.gym_api.domain.dtos.membershipType;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,9 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class MembershipTypeRequest {
+	@NotNull
 	private String type;
+
+	@NotNull
 	private Float price;
 }
