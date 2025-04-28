6bd5b826b56f48dce6f0aba401a8d8ef5320a0c4
Filip Stępień <filip.stepien30@gmail.com>
2025-04-28 22:37:23 +0200

    feat: payment statuses endpoint

---
+++ src/src/test/java/xyz/cursedman/gym_api/controllers/PaymentStatusControllerTest.java
@@ -0,0 +1,29 @@
+package xyz.cursedman.gym_api.controllers;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class PaymentStatusControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	private final String endpointUri = "/payment-statuses";
+
+	@Test
+	void checkIfGetReturnsHttp200AndAllRecords() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get(endpointUri))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.length()", Matchers.greaterThan(0)));
+	}
+}
