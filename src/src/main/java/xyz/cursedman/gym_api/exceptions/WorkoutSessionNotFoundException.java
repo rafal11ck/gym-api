78783020ce89e3f43b554ffa6d3c7b2633573d73
rafal <you@example.com>
2025-05-21 10:59:07 +0200

    fix: use lombok for exception methods

---
+++ src/src/main/java/xyz/cursedman/gym_api/exceptions/WorkoutSessionNotFoundException.java
@@ -0,0 +1,10 @@
+package xyz.cursedman.gym_api.exceptions;
+
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class WorkoutSessionNotFoundException extends WorkoutSessionException {
+
+
+}
