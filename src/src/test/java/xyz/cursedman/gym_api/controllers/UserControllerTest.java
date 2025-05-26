b10e48ebf7e6c34d432e058b93ea13e629749e75
rafal <you@example.com>
2025-05-26 20:19:51 +0200

    chore: remove old role dto remains

--- src/src/test/java/xyz/cursedman/gym_api/controllers/UserControllerTest.java
+++ src/src/test/java/xyz/cursedman/gym_api/controllers/UserControllerTest.java
@@ -26,19 +26,10 @@ import java.util.UUID;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class UserControllerTest {
 
-	@Autowired
-	private MockMvc mockMvc;
-
 	private final String endpointUri = "/users";
-
 	private final String validUserUuid = "65f40335-135a-47ec-ad7d-72278c4be65c";
-
 	private final String validCardUuid = "5bd05494-155e-4bd1-b14c-61421d0caaae";
-
 	private final String validMembershipUuid = "ddd5f2a7-157e-4bf1-b11c-fa46e0d6bad1";
-
-	private final String validRoleUuid = "0608911b-f8fa-4a83-8f63-f314030a36ed";
-
 	private final UserRequest validUserRequest = UserRequest.builder()
 		.firstName("John")
 		.lastName("Pork")
@@ -48,8 +39,9 @@ class UserControllerTest {
 		.phoneNumber("123123123")
 		.cardUuid(UUID.fromString(validCardUuid))
 		.membershipUuid(UUID.fromString(validMembershipUuid))
-		.roleUuid(UUID.fromString(validRoleUuid))
 		.build();
+	@Autowired
+	private MockMvc mockMvc;
 
 	// GET
 
@@ -77,22 +69,20 @@ class UserControllerTest {
 
 	@Test
 	void checkIfCreateUserReturnsHttp201AndCreatedRecord() throws Exception {
-		String[] fieldsToIgnore = { "membershipUuid", "cardUuid", "roleUuid" };
+		String[] fieldsToIgnore = {"membershipUuid", "cardUuid", "roleUuid"};
 		mockMvc.perform(
-			MockMvcRequestBuilders.post(endpointUri)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(TestJsonHelper.stringify(validUserRequest))
-		).andExpect(MockMvcResultMatchers.status().isCreated())
-		.andExpect(TestJsonHelper.contentEqualsJsonOf(validUserRequest, fieldsToIgnore))
-		.andExpect(
-			MockMvcResultMatchers.jsonPath("$.membership.uuid", Matchers.is(validMembershipUuid))
-		)
-		.andExpect(
-			MockMvcResultMatchers.jsonPath("$.card.uuid", Matchers.is(validCardUuid))
-		)
-		.andExpect(
-			MockMvcResultMatchers.jsonPath("$.role.uuid", Matchers.is(validRoleUuid))
-		);
+				MockMvcRequestBuilders.post(endpointUri)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(TestJsonHelper.stringify(validUserRequest))
+			).andExpect(MockMvcResultMatchers.status().isCreated())
+			.andExpect(TestJsonHelper.contentEqualsJsonOf(validUserRequest, fieldsToIgnore))
+			.andExpect(
+				MockMvcResultMatchers.jsonPath("$.membership.uuid", Matchers.is(validMembershipUuid))
+			)
+			.andExpect(
+				MockMvcResultMatchers.jsonPath("$.card.uuid", Matchers.is(validCardUuid))
+			);
+
 	}
 
 	@Test
@@ -110,13 +100,13 @@ class UserControllerTest {
 	void checkIfUserPatchUpdateReturnsHttp200AndUpdatedRecord() throws Exception {
 		String cardUuidToUpdate = "ed36b15c-89d7-43cd-aa1b-354b2ec9067d";
 		mockMvc.perform(
-			MockMvcRequestBuilders.patch(endpointUri + "/" + validUserUuid)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(TestJsonHelper.toJSONField("cardUuid", cardUuidToUpdate))
-		).andExpect(MockMvcResultMatchers.status().isOk())
-		.andExpect(
-			MockMvcResultMatchers.jsonPath("$.card.uuid", Matchers.is(cardUuidToUpdate))
-		);
+				MockMvcRequestBuilders.patch(endpointUri + "/" + validUserUuid)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(TestJsonHelper.toJSONField("cardUuid", cardUuidToUpdate))
+			).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(
+				MockMvcResultMatchers.jsonPath("$.card.uuid", Matchers.is(cardUuidToUpdate))
+			);
 	}
 
 	@Test
@@ -135,11 +125,11 @@ class UserControllerTest {
 	@Test
 	void checkIfGetUserChatsReturnsHttp200AndAllRecords() throws Exception {
 		mockMvc.perform(
-			MockMvcRequestBuilders.get(endpointUri + "/" + validUserUuid + "/chats")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(TestJsonHelper.stringify(validUserRequest))
-		).andExpect(MockMvcResultMatchers.status().isOk())
-		.andExpect(MockMvcResultMatchers.jsonPath("$.length()", Matchers.greaterThan(0)));
+				MockMvcRequestBuilders.get(endpointUri + "/" + validUserUuid + "/chats")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(TestJsonHelper.stringify(validUserRequest))
+			).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.length()", Matchers.greaterThan(0)));
 	}
 
 	@Test
