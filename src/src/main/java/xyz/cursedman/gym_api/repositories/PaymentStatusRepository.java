f71ee0769d93b1c3ddb3a9ae8ccb426bba0bdff8
rafal <you@example.com>
2025-06-02 20:25:08 +0200

    chore: clean old

--- src/src/main/java/xyz/cursedman/gym_api/repositories/PaymentStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package xyz.cursedman.gym_api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import xyz.cursedman.gym_api.domain.entities.PaymentStatus;
-
-import java.util.UUID;
-
-public interface PaymentStatusRepository extends JpaRepository<PaymentStatus, UUID> {
-}
